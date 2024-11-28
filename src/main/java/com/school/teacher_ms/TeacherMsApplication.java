0368a22646e0e3780095e375ab7efdeacc18dc34
Tedros Alem <tedsaasfaha@gmail.com>
2024-11-28 22:00:22 +0300

    first commit - project initialization

---
+++ src/main/java/com/school/teacher_ms/TeacherMsApplication.java
@@ -0,0 +1,13 @@
+package com.school.teacher_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/teacher_ms/TeacherMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.teacher_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
